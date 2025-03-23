1942ab04b29d1299aa019cea2dd8d3e370ca00e4
Cedor <cedordark@yahoo.fr>
2025-03-23 17:32:44 +0100

    add tile changes for CCA

---
+++ crawlcosplay.lua
@@ -0,0 +1,42 @@
+-----------------------------
+---- Begin crawl_cosplay ----
+-----------------------------
+if you.race() == "Troll" and you.class() == "Berserker" then
+    crawl.setopt("tile_player_tile = mons:Snorg")
+end
+if you.race() == "Minotaur" and you.class() == "Fighter" then
+    crawl.setopt("tile_player_tile = mons:Asterion")
+end
+if you.race() == "Mountain Dwarf" and you.class() == "Gladiator" then
+    crawl.setopt("tile_player_tile = mons:Wiglaf")
+end
+if you.race() == "Merfolk" and you.class() == "Monk" then
+    crawl.setopt("tile_player_tile = mons:Donald")
+end
+if you.race() == "Kobold" and you.class() == "Brigand" then
+    crawl.setopt("tile_player_tile = mons:Sonja")
+end
+if you.race() == "Naga" and you.class() == "Hunter" then
+    crawl.setopt("tile_player_tile = mons:Vashnia")
+end
+if you.race() == "Demigod" and you.class() == "Conjurer" then
+    crawl.setopt("tile_player_tile = mons:Frederick")
+end
+if you.race() == "Oni" and you.class() == "Ice Elementalist" then
+    crawl.setopt("tile_player_tile = mons:Antaeus")
+end
+if you.race() == "Gargoyle" and you.class() == "Earth Elementalist" then
+    crawl.setopt("tile_player_tile = mons:Roxanne")
+end
+if you.race() == "Barachi" and you.class() == "Summoner" then
+    crawl.setopt("tile_player_tile = mons:Jeremiah")
+end
+if you.race() == "Draconian" and you.class() == "Shapeshifter" then
+    crawl.setopt("tile_player_tile = mons:Bai Suzhen")
+end
+if you.race() == "Spriggan" and you.class() == "Enchanter" then
+    crawl.setopt("tile_player_tile = mons:The Enchantress")
+end
+-----------------------------
+----  End crawl_cosplay  ----
+-----------------------------
